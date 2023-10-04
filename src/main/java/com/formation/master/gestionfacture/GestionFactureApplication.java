a01ba6d2b7f03cb6bab3656dc8e4696ad2156e39
ijawhar <ikram.jawhar@gmail.com>
2023-10-04 11:33:44 +0200

    Etape 1 project gestion facture java 17 et spring 3

---
+++ src/main/java/com/formation/master/gestionfacture/GestionFactureApplication.java
@@ -0,0 +1,13 @@
+package com.formation.master.gestionfacture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionFactureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionFactureApplication.class, args);
+    }
+
+}
